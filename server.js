bd4fce9200f57d44e25813491d1cb59e4dec106b
ayush109-2528 <ayushrajput109@gmail.com>
2026-02-03 14:36:24 +0530

    update file

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors()); // Allow all origins for dev, or configure for production
+app.use(cors({
+    origin: 'https://cloud-storage-orcin.vercel.app', 
+    credentials: true
+})); // Allow all origins for dev, or configure for production
 app.use(express.json());
 
 // Routes
@@ -23,3 +26,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+module.exports = app;
